baf8a9ed64bb04c53f96d0c82f4fb3b2a18a3777
kallepakarinen <mikko.kalle.pakarinen@gmail.com>
2019-10-04 18:21:13 +0300

    Created new components and include dependencys

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PresentationComponent } from './presentation/presentation.component';
+import { HeaderComponent } from './presentation/header/header.component';
+import { ContainerComponent } from './presentation/container/container.component';
+import { FooterComponent } from './presentation/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PresentationComponent,
+    HeaderComponent,
+    ContainerComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
